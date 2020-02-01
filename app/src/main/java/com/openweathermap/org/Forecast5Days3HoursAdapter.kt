7520a4d37a216013c3c789f67354faea6292396d
maqsood <maqsoodzai1989@gmail.com>
2020-02-01 17:58:52 +0530

    Working on beautification UI

--- app/src/main/java/com/openweathermap/org/Forecast5Days3HoursAdapter.kt
+++ app/src/main/java/com/openweathermap/org/Forecast5Days3HoursAdapter.kt
@@ -4,7 +4,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.openweathermap.org.model.ListItem
 import kotlinx.android.synthetic.main.forecast_row.view.*
 
